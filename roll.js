b2cd077712a3b303df6806fe523c454c517dedec
Abirami2005 <115530037+Abirami2005@users.noreply.github.com>
2024-12-27 14:30:28 +0530

    Add files via upload

---
+++ roll.js
@@ -0,0 +1,8 @@
+var f=Math.floor(Math.random()*6)+1;
+var l=Math.floor(Math.random()*6)+1;
+var j=Math.floor(Math.random()*6)+1;
+document.getElementById("de").onclick=function(){
+document.getElementById("b").textContent=f;
+document.getElementById("a").textContent=l;
+document.getElementById("c").textContent=j;
+}
